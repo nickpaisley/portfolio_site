f910fd888cf594936daa73e953af578df8d1df05
Nick Paisley <2ep303@gmail.com>
2019-09-14 17:49:24 -0500

    added pic modal

--- projects/apod/js/apod.js
+++ projects/apod/js/apod.js
@@ -30,3 +30,24 @@ req.addEventListener("load", function () {
     }
 
 })
+
+// Get the modal
+var modal = document.getElementById("picModal");
+
+// Get the image and insert it inside the modal - the "alt" as the caption
+var img = document.getElementById("pic");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt;
+}
+
+//get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+//When the user clicks on the <span> (x), close the modal
+span.onclick = function(){
+    modal.style.display = "none";
+}
